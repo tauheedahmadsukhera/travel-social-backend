7c2ae2720f4497db794969534554e574629ced64
tauhee56 <tauheedjunior@gmail.com>
2026-05-02 14:27:31 +0500

    Fix Cloudinary config in upload.js

--- routes/upload.js
+++ routes/upload.js
@@ -3,6 +3,14 @@ const router = express.Router();
 const cloudinary = require('cloudinary').v2;
 const multer = require('multer');
 
+// Configure Cloudinary
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET
+});
+
+
 // Configure multer for memory storage
 const upload = multer({ 
   storage: multer.memoryStorage(),
